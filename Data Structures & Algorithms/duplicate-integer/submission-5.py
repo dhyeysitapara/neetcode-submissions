fcb7610f3efe207c0f0b5487e3c44733ca181ebd
Dhyey Sitapara <dhyey.sitapara48@gmail.com>
2026-05-26 15:33:48 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        freq = {}
+
+        for i in nums:
+            freq[i]  = freq.get(i,0) + 1
+
+            if freq[i] == 2:
+                return True
+                break
+
+        else:
+            return False
+        
