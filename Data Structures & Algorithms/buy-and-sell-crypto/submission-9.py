ce72375cd33e370fbda764fe9a34b5ed3aca7461
Dhyey Sitapara <dhyey.sitapara48@gmail.com>
2026-06-16 10:49:09 +0530

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        buy = 0 
+        sell = 1
+        max_profit = 0
+
+        while sell < len(prices):
+            if prices[sell] < prices[buy]:
+                buy = sell
+
+            else:
+                profit = prices[sell] - prices[buy]
+
+                max_profit = max(profit,max_profit)
+
+            sell += 1
+
+        return max_profit
+        
